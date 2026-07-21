52d06ee6fb86300c63a694e8ffb5531949f759a0
soyr_6o <soyrozero@gmail.com>
2026-07-21 07:29:11 +0330

    fix(frontend):update url api

--- frontend/src/redux/slices/apiSlice.ts
+++ frontend/src/redux/slices/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000",
+    baseUrl: import.meta.env.VITE_GO_API_URL,
     credentials: "include",
   }),
   tagTypes: ["Product", "Order", "User"],
